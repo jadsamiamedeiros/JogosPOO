abef62b8ab929dc4c0fdccac31b3753873cb3e21
Jade medeiros <jadsamiamedeiros11@gmail.com>
2016-06-09 13:24:15 -0300

    Update HelloWord.cpp

--- HelloWord.cpp
+++ HelloWord.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char* args[])
 {
-	cout << "Hello Word" << endl; // imprimi a frase na tela
+	cout << "Hello Word" << endl; // imprimi a frase na tela e dá uma quuebra de linha em seguida
 	return 0; //finaliza o programa
 }
 
