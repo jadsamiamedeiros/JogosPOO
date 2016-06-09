1d8c50a02758feb1af9b6aee688ad8317019659b
Jade medeiros <jadsamiamedeiros11@gmail.com>
2016-06-09 14:10:49 -0300

    Update TesteDeForça.cpp

--- TesteDeForça.cpp
+++ TesteDeForça.cpp
@@ -20,8 +20,7 @@ int main(int argc, char* args[])
 	cout << "Digite o nome do jogador 2: " << endl; 
 	cin >> jog2;
 
-	while (sair == false) //laço/comando
-	enquanto, que se encontra a variavel booleano que ao ser falso finaliza o laço de repetição 
+	while (sair == false) //laço enquanto, que se encontra a variavel booleano que ao ser falso finaliza o laço de repetição 
 	{
 		cout << " o                  o" << endl; // desenhos que vão ser imprimidos na tela e ao mesmo tempo dá uma quebra de linha
 		cout << ".T./              \\.T." << endl;
